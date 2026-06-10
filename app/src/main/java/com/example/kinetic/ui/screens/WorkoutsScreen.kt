d48811ab8f81e7ce10944bf1c751aa77fd63b340
ADAM-DESKTOP <adam14desktop@gmail.com>
2026-06-10 22:00:49 +0300

    new edits for suceed compilation

--- app/src/main/java/com/example/kinetic/ui/screens/WorkoutsScreen.kt
+++ app/src/main/java/com/example/kinetic/ui/screens/WorkoutsScreen.kt
@@ -25,6 +25,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -34,7 +36,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun WorkoutsListScreen(
     state: WorkoutListState,
